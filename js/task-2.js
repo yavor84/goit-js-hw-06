acf31373fccc251f1f974be53d127cb725ebc0dd
Dmytro Yavor <pojardim@gmail.com>
2024-12-08 13:47:27 +0200

    add tasks

---
+++ js/task-2.js
@@ -0,0 +1,29 @@
+class Storage {
+  #items;
+  constructor(items) {
+    this.#items = items;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    if (this.#items.indexOf(itemToRemove) !== -1) {
+      this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+    }
+  }
+}
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem('Scaner');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
